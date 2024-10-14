2ff1e8daa819516dc0c5c765c102a80bc92c7f86
Pavel Gordienko <guzmo@yandex.ru>
2024-10-14 22:26:14 +0300

    feat: add welcome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const start = () => {
-    console.log('hello')
+
+    function getUsername() {
+        const args = process.argv.slice(2);
+        const usernameArg = args.find(arg => arg.startsWith('--username='));
+
+        if (usernameArg) {
+            return usernameArg.split('=')[1];
+        }
+
+        return 'Nobody';
+    }
+    const username = getUsername();
+    console.log(`Welcome to the File Manager, ${username}!`);
+
 }
 
 start();
